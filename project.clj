6ded1e65ac0fdc80a28195377972ca119d7502cf
Richard Möhn <richard.moehn@posteo.de>
2017-03-15 11:06:40 +0900

    project.clj: Remove specter

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
-                 [com.rpl/specter "1.0.0"]
                  [enlive "1.1.6"]
                  [twitter-api "1.8.0"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
